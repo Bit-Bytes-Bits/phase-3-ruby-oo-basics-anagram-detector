75998ab05fdab27daf7262fabe1bc15945009411
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-27 22:38:13 +0300

    Done with assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    my_anagram = array.select { |str|
+      str.split("").sort == @name.split("").sort
+    }
+    return my_anagram
+  end
+end
